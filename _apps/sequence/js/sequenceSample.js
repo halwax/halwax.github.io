f75114261706bfe9bbe06cb6d7127120818f6f34
Michael Halwax <mhalwax@gmail.com>
2020-02-24 08:37:50 +0100

    improve sequence sample

--- _apps/sequence/js/sequenceSample.js
+++ _apps/sequence/js/sequenceSample.js
@@ -1,9 +1,11 @@
 const tsDefaultSequence = 
-`let user = actor('User');
-let controller = actor('Controller');
-let service = actor('Service');
+`let user = actor(\`User\`);
+let controller = actor(\`Controller\`);
+let service = actor(\`Service\`);
 
-user.send(controller, '/rest/endpoint')
-        .send(service, 'serviceMethod(...)')
+user
+    .send(controller, \`/rest/endpoint\`)
+        .send(service, \`serviceMethod(...)\`)
+        .info(\`calculates result\`)
         .respond()
     .respond();`;
